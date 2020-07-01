65cdb5d2327f17a0403d647f8dbb9b9b8c002ca1
Ljl <L@AAAAAMacBook-Pro.local>
2020-07-01 15:33:59 +0800

    首页增加导航滑动效果

--- News_project/pages/index/index.vue
+++ News_project/pages/index/index.vue
@@ -1,13 +1,32 @@
 <template>
 	<view>
-		 <uniSegmentedControl 
+	<!-- 	 <uniSegmentedControl 
 		 :current="current" 
 		 :values="items" 
 		 @clickItem="onClickItem" 
 		 active-color="#f15a22"
 		 style-type="text">
-		 </uniSegmentedControl>
-
+		 </uniSegmentedControl> -->
+		<scroll-view 
+		scroll-x="true" 
+		class="header">
+			<view class="header_item" v-for="(item , index) in items" :key = "index">
+				<text 
+				@click="handelHeader(index)"
+				:style="{
+					color: index === current ? '#F15A22':'#333',
+					fontSize: index === current ? '50upx':'30upx',
+				}"
+				>{{item}}</text>
+				<view class="heade_line" 
+				:style="{
+					backgroundColor: index === current ? '#F15A22':'#fff'
+				}">
+				</view>
+				
+			</view>
+		</scroll-view>
+		
 			
 	<!-- <newslistView></newslistView> -->
 	</view>
@@ -30,7 +49,12 @@
 					'经济',
 					'旅游',
 					'文学',
-					'图片'
+					'图片',
+					'图片',
+					'图片',
+					'图片',
+					'图片',
+					
 				],
 				current:0
 			}
@@ -39,7 +63,10 @@
 			
 		},
 		methods: {
-			
+			handelHeader(num){
+				console.log(num);
+				this.current = num
+			}
 		},
 		components:{
 			newslistView,
@@ -50,6 +77,35 @@
 	
 </script>
 
-<style>
+<style lang="scss">
+	.header{
+		width: 100%;
+		
+		// overflow: hidden;
+		// // overflow-y: hidden;
+		white-space: nowrap;
+		height: 100rpx;
+		
+		.header_item{
+			display: inline-block;
+			width: 150rpx;
+			// height: 80upx;
+			border:  1rpx solid #FFFFFF;;
+			text-align: center;
+			font-size: 32upx;
+		}
+		.heade_line{
+			
+			background-color: #F15A22;
+			height: 5upx;
+			width: 150rpx;
+			margin-left: 1rpx;
+			margin-top: 8rpx;
+			
+		}
+		::-webkit-scrollbar {
+			display: none;
+		}
+	}
 	
 </style>
---
+++ test/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<view class="green">
+			A
+		</view>
+		<view class="red">
+			B
+		</view>
+		<view class="yellow">
+			C
+		</view>
+		<view class="green">
+			D
+		</view>
+		
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css");
+</style>
